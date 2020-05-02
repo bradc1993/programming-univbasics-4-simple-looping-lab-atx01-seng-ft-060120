967d6e17f6feae026cc67056b616f4e07ed93522
Brad Carter <bradc1993@gmail.com>
2020-05-02 02:14:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,22 @@
 # Write your methods here
+
+def loop_message_five_times(string)
+  counter = 0
+  while counter < 5 do
+    puts string
+    counter += 1
+  end
+end
+
+def loop_message_n_times(string, n)
+  counter = 0
+  while counter < n do
+    puts string
+    counter += 1
+  end
+end
+
+def output_array(array)
+  counter = 0
+  while counter < array.length do
+    puts
